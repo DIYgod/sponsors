bc3a764a22ef564c93b849abd99c4ac96929f8d2
DIYgod <i@diygod.me>
2024-03-03 00:51:43 +0800

    feat: tiers

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -12,31 +12,21 @@ export default defineConfig({
       preset: presets.xs,
     },
     {
-      title: 'Backers',
+      title: 'Donors',
       preset: presets.base,
     },
     {
-      title: 'Sponsors',
-      monthlyDollars: 8,
+      title: 'Appreciators',
+      monthlyDollars: 5,
       preset: presets.medium,
       composeAfter: (composer, tierSponsors, config) => {
         composer.addSpan(10)
       },
     },
     {
-      title: 'Bronze Sponsors',
-      monthlyDollars: 32,
+      title: 'Supporters',
+      monthlyDollars: 8,
       preset: presets.large,
-    },
-    {
-      title: 'Silver Sponsors',
-      monthlyDollars: 64,
-      preset: presets.xl,
-    },
-    {
-      title: 'Gold Sponsors',
-      monthlyDollars: 256,
-      preset: presets.xl,
     },
   ],
 })
